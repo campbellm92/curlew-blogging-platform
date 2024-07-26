7aac2e476810b1f314f17ed92968f202c04f4a1d
Matthew Campbell <m.campbell92@gmail.com>
2024-07-26 14:02:51 +0200

    remove comments

--- public/scripts/loadMarkdown.js
+++ public/scripts/loadMarkdown.js
@@ -1,17 +1,3 @@
-// async function loadMarkdown(selector, url) {
-//   try {
-//     const response = await fetch(url);
-//     if (!response) {
-//       throw new Error("Network not responding.");
-//     }
-//     const markdownContent = await response.text();
-//     const htmlContent = marked(markdownContent);
-//     document.querySelector(selector).innerHTML = htmlContent;
-//   } catch (error) {
-//     console.log("Error loading markdown", error);
-//   }
-// }
-
 document.addEventListener("DOMContentLoaded", async () => {
   const urlParams = new URLSearchParams(window.location.search);
   const postId = urlParams.get("file");
