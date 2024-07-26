1ca703753be92f2f2220da17cd3de3c5adc4f030
Matthew Campbell <m.campbell92@gmail.com>
2024-07-26 14:02:13 +0200

    remove redundant logic

--- public/scripts/loadMarkdown.js
+++ public/scripts/loadMarkdown.js
@@ -1,18 +1,23 @@
-async function loadMarkdown(selector, url) {
-  try {
-    const response = await fetch(url);
-    if (!response) {
-      throw new Error("Network not responding.");
-    }
-    const markdownContent = await response.text();
-    const htmlContent = marked(markdownContent);
-    document.querySelector(selector).innerHTML = htmlContent;
-  } catch (error) {
-    console.log("Error loading markdown", error);
-  }
-}
+// async function loadMarkdown(selector, url) {
+//   try {
+//     const response = await fetch(url);
+//     if (!response) {
+//       throw new Error("Network not responding.");
+//     }
+//     const markdownContent = await response.text();
+//     const htmlContent = marked(markdownContent);
+//     document.querySelector(selector).innerHTML = htmlContent;
+//   } catch (error) {
+//     console.log("Error loading markdown", error);
+//   }
+// }
 
 document.addEventListener("DOMContentLoaded", async () => {
-  const postId = new URLSearchParams(window.location.search).get("post");
-  await loadMarkdown(".main-content", `/post/${postId}`);
+  const urlParams = new URLSearchParams(window.location.search);
+  const postId = urlParams.get("file");
+  if (postId) {
+    const response = await fetch(`/posts/${postId}`);
+    const markdownContent = await response.text();
+    document.querySelector(".main-content").innerHTML = markdownContent;
+  }
 });
