97cf6bd34f0b109c002ce32b6a885ad6e9853032
Matthew Campbell <m.campbell92@gmail.com>
2024-07-26 17:17:36 +0200

    add code for loading hljs

--- public/scripts/loadMarkdown.js
+++ public/scripts/loadMarkdown.js
@@ -2,8 +2,18 @@ document.addEventListener("DOMContentLoaded", async () => {
   const urlParams = new URLSearchParams(window.location.search);
   const postId = urlParams.get("file");
   if (postId) {
-    const response = await fetch(`/posts/${postId}`);
-    const markdownContent = await response.text();
-    document.querySelector(".main-content").innerHTML = markdownContent;
+    try {
+      const response = await fetch(`/posts/${postId}`);
+      if (!response) {
+        throw new Error("Failed to fetch post content");
+      }
+      const markdownContent = await response.text();
+      document.querySelector(".main-content").innerHTML = markdownContent;
+      document.querySelectorAll("pre code").forEach((block) => {
+        hljs.highlightBlock(block);
+      });
+    } catch (error) {
+      console.log("Error loading post", error);
+    }
   }
 });
