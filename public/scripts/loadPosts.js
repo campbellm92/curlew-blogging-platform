7b724747386f619fd5c77240574d4d65869c479e
Matthew Campbell <m.campbell92@gmail.com>
2024-07-26 14:03:28 +0200

    fix link to handle post files

--- public/scripts/loadPosts.js
+++ public/scripts/loadPosts.js
@@ -1,13 +1,13 @@
 document.addEventListener("DOMContentLoaded", async () => {
   try {
-    const response = await fetch("../posts");
+    const response = await fetch("/posts-list");
     const posts = await response.json();
 
-    const postList = document.querySelector(".post-list");
+    const postList = document.querySelector(".posts-list");
     posts.forEach((post) => {
       const postItem = document.createElement("li");
       const a = document.createElement("a");
-      a.href = `post.html?post=${post.file}`;
+      a.href = `/post?file=${post.file}`;
       a.textContent = post.title;
       postItem.appendChild(a);
       postList.appendChild(postItem);
